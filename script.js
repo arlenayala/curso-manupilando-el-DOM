390ee7c2689aaad97639e59158eebfd4115ad030
arlen ayala <arlenayala1992@gmail.com>
2023-06-30 00:16:27 -0500

    clase03 completada

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const btn = document.querySelector("[data-form-btn]");
 
-console.log(btn);
 
 
 const createTask = (evento)=>{
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
+    const value= input.value;
+    input.value="";
+    const task= document.createElement("li");
+    const list=document.querySelector("[data-list]")
     
-    console.log(input.value);
+    const content= `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  console.log(content);
+  task.innerHTML=content;
+  task.classList.add("card");
+  list.appendChild(task);
 }
 
 btn.addEventListener("click", createTask);
